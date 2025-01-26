9ff8d952311d8199cd15f5605ca3f7ac8eacff87
DonMB77 <purposefuldrifter@gmail.com>
2025-01-26 15:33:46 +0100

    awaitility dep. + tests removal of Thread.sleep()

--- src/test/java/com/drifter/spring_6_webclient/client/BeerClientImplTest.java
+++ src/test/java/com/drifter/spring_6_webclient/client/BeerClientImplTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -14,29 +17,25 @@ class BeerClientImplTest {
 
     @Test
     void listBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
         beerClient.listBeer().subscribe(response -> {
             System.out.println(response);
+            atomicBoolean.set(true);
         });
 
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        await().untilTrue(atomicBoolean);
     }
 
     @Test
     void listBeerMap() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
         beerClient.listBeerMap().subscribe(response -> {
             System.out.println(response);
+            atomicBoolean.set(true);
         });
 
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        await().untilTrue(atomicBoolean);
     }
 }
